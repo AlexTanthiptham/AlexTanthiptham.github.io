7255c7173c33ca8d7b83c0f88644f2f488a5b319
Alex Tanthiptham <jirawatcharat@gmail.com>
2024-06-11 16:13:01 -0500

    Minor update to reflect WIP status of site

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -1,26 +1,34 @@
 import React from "react";
-import logo from "../logo.svg";
+import roadworks from "../assets/roadworks.png";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. Home Page.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={roadworks} className="App-logo" alt="logo" />
+        <p>⚠️ This site is currently under construction! ⚠️</p>
+        <p>Please check back later!</p>
       </header>
     </div>
   );
 };
 
 export default Home;
+
+// <div className="App">
+// <header className="App-header">
+//   <img src={logo} className="App-logo" alt="logo" />
+//   <p>
+//     Edit <code>src/App.tsx</code> and save to reload. Home Page.
+//   </p>
+//   <a
+//     className="App-link"
+//     href="https://reactjs.org"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >
+//     Learn React
+//   </a>
+// </header>
+// </div>
