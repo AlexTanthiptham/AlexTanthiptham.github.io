ef7317cb88355f0b49e483d3da2f856b6a37a6b4
Alex Tanthiptham <jirawatcharat@gmail.com>
2024-06-11 15:44:38 -0500

    Initial react-router-dom setup, pushing to test gh-pages

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+
+// const TopNav = () => (
+//   <nav>
+//     <ul>
+//       <li>
+//         <a href="/">Home</a>
+//       </li>
+//       <li>
+//         <a href="/about">About</a>
+//       </li>
+//     </ul>
+//   </nav>
+// );
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />,
+      <Route path="/Home" element={<Home />} />,
+    </Routes>
   );
 }
 
