34471673272931c766146076d7050b4d1262862e
Alex Tanthiptham <jirawatcharat@gmail.com>
2024-07-02 14:50:22 -0500

    Functional implementation of email service

--- portfolio/src/components/SectionContacts.tsx
+++ portfolio/src/components/SectionContacts.tsx
@@ -1,25 +1,110 @@
+import React, { useRef } from "react";
+
 import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import { Send, Delete } from "@mui/icons-material/";
+import emailjs from "@emailjs/browser";
 
 import StyleWrapper from "./StyleWrapper";
 
 const SectionContacts = () => {
+  // Insert function for sending email here
+  const form = useRef<HTMLFormElement>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm(
+          "contact_service",
+          "template_5qhr88b",
+          form.current,
+          "H_ylPPVBqXXYxaOOb"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            alert("Message sent successfully!");
+          },
+          (error) => {
+            console.log(error.text);
+            alert("Failed to send message. Please try again later.");
+          }
+        );
+    }
+  };
+
   return (
     <StyleWrapper>
       <Container id="contact" maxWidth="lg" className="stackedText">
-        <Typography variant="h3" className="sectionHeader">
-          Contact
-        </Typography>
-        <Typography variant="body1" className="sectionHeader" align="center">
-          Let's talk! I'm reachable at jt20@illinois.edu or through the form
-          below
-        </Typography>
-        <TextField id="contact-name" label="Name" variant="outlined" />
-        <TextField id="contact-email" label="Email" variant="outlined" />
-        <TextField id="contact-send" label="Message" variant="outlined" />
-        <Button id="contact-submit" variant="contained">
-          {" "}
-          Submit{" "}
-        </Button>
+        <Container
+          component="form"
+          ref={form}
+          onSubmit={sendEmail}
+          maxWidth="md"
+          sx={{
+            "& > *": {
+              py: 3,
+            },
+          }}
+        >
+          <Typography variant="h3" className="sectionHeader">
+            Contact
+          </Typography>
+          <Typography variant="body1" className="textInika" align="center">
+            Let's talk! I'm reachable at jt20@illinois.edu or through the form
+            below
+          </Typography>
+          <TextField
+            label="Your Name"
+            name="user_name"
+            variant="outlined"
+            fullWidth={true}
+            className="textInika"
+            required={true}
+          />
+          <TextField
+            name="user_email"
+            label="Your Email Address"
+            variant="outlined"
+            fullWidth={true}
+            className="textInika"
+            required={true}
+          />
+          <TextField
+            name="message"
+            label="Your Message"
+            variant="outlined"
+            fullWidth={true}
+            className="textInika"
+            required={true}
+          />
+          <Container
+            sx={{
+              alignItems: "center",
+              "& > *": {
+                mx: 3,
+              },
+            }}
+          >
+            <Button
+              id="contact-submit"
+              variant="contained"
+              endIcon={<Send />}
+              type="submit"
+            >
+              Submit
+            </Button>
+            <Button
+              id="contact-clear"
+              variant="contained"
+              endIcon={<Delete />}
+              type="reset"
+            >
+              Clear
+            </Button>
+          </Container>
+        </Container>
       </Container>
     </StyleWrapper>
   );
