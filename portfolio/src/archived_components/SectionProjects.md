da190b04340885422df0b47d88b6b6ef0f58e4b3
Alex Tanthiptham <jirawatcharat@gmail.com>
2025-05-19 20:15:20 -0500

    Projects Redesign

---
+++ portfolio/src/archived_components/SectionProjects.md
@@ -0,0 +1,177 @@
+import { Box, Grid, Container, Typography } from "@mui/material";
+
+import StyleWrapper from "./UtilsStyleWrapper";
+
+// TODO: Clean up image imports
+import projectBiped from "../assets/project-icons/project-biped.png";
+import projectGemev from "../assets/project-icons/project-gemev.jpg";
+import projectTagAlong from "../assets/project-icons/project-tagalong.png";
+import projectVisat from "../assets/project-icons/project-visat.png";
+import projectLlamaBear from "../assets/project-icons/project-llamabear.png";
+import projectIlliniAlert from "../assets/project-icons/project-illinialert.png";
+
+const SectionProjects = () => {
+  return (
+    <Container id="projects" maxWidth="lg" className="textPrimary">
+      <StyleWrapper>
+        <Typography variant="h3" className="sectionHeader">
+          Projects
+        </Typography>
+        <Typography
+          variant="body1"
+          className="fontPrimary"
+          align="center"
+          sx={{ marginBottom: 2 }}
+        >
+          Note: This section is a work in progress. In the near future, project
+          tiles will be clickable to display more information.
+        </Typography>
+        <Grid container spacing={0}>
+          <Grid item xs={12} sm={6} md={3}>
+            <ProjectTile
+              image={projectTagAlong}
+              title={"TagAlong Robot"}
+              description={
+                "User-following personal assistant for disabled users"
+              }
+              tags={["PCB Design"]}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <ProjectTile
+              image={projectGemev}
+              title={"Lane Following for GEM e2"}
+              description={"Lane following for GEM e2 electric vehicle"}
+              tags={["Python, OpenCV"]}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <ProjectTile
+              image={projectBiped}
+              title={"CPSIL - BIPED"}
+              description={
+                "Two wheeled self-balancing robots for CS431 Embedded Systems and as a research platform for CPSIL Visat "
+              }
+              tags={["Python"]}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <ProjectTile
+              image={projectVisat}
+              title={"CPSIL - VISAT"}
+              description={
+                "User-following personal assistant for disabled users"
+              }
+              tags={["Python, Pytorch, OpenCV"]}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <ProjectTile
+              image={projectLlamaBear}
+              title={"LlamaBear"}
+              description={
+                "MERN stack web app for reviewing and improving resumes"
+              }
+              tags={["React, Node.js, Express, MongoDB, Firebase, Figma"]}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <ProjectTile
+              image={projectIlliniAlert}
+              title={"IlliniAlert App"}
+              description={
+                "Functional prototype for a campus safety app, visualizing crimes and hazards relative to the user's location"
+              }
+              tags={["Android Studio, Java, Google Maps API, Figma"]}
+            />
+          </Grid>
+        </Grid>
+      </StyleWrapper>
+    </Container>
+  );
+};
+
+export default SectionProjects;
+
+// [Helper] ProjectTile.tsx
+interface ProjectTileProps {
+  image: string; // Imported images are strings of the filepath
+  title: string;
+  description: string;
+  tags: string[];
+}
+
+const ProjectTile: React.FC<ProjectTileProps> = ({
+  image,
+  title,
+  description,
+  tags,
+}) => {
+  return (
+    <Box
+      id="projectContainer"
+      sx={{
+        position: "relative",
+        width: "100%",
+        paddingTop: "100%", // 1:1 aspect ratio
+        overflow: "hidden",
+      }}
+    >
+      <StyleWrapper>
+        <Box
+          id="projectImage"
+          component="img"
+          src={image}
+          alt={title}
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            width: "100%",
+            height: "100%",
+            objectFit: "contain",
+            transform: "translate(-50%, -50%)",
+          }}
+        />
+        <Box
+          id="projectHoverSection"
+          sx={{
+            position: "absolute",
+            top: 0,
+            background: "rgba(0, 0, 0, 0.5)",
+            color: "white",
+            boxSizing: "border-box",
+            height: "100%",
+            width: "100%",
+            opacity: 0,
+            padding: "5%",
+            transition: "opacity 0.3s",
+            "&:hover": {
+              opacity: 1,
+            },
+            textWrap: "wrap",
+          }}
+        >
+          <Typography
+            variant="h4"
+            className="fontPrimary textBold textSecondary"
+          >
+            {title}
+          </Typography>
+          <Typography variant="h6" className="fontPrimary textSecondary">
+            {description}
+          </Typography>
+          <Typography
+            variant="body2"
+            className="fontPrimary textSecondary textUnderline textBold"
+          >
+            Tags:
+          </Typography>
+          <Typography variant="body2" className="fontPrimary textSecondary">
+            {tags.join(", ")}
+          </Typography>
+        </Box>
+      </StyleWrapper>
+    </Box>
+  );
+};
--- portfolio/src/components/SectionProjects.tsx
+++ portfolio/src/components/SectionProjects.tsx
@@ -5,10 +5,11 @@ import StyleWrapper from "./UtilsStyleWrapper";
 // TODO: Clean up image imports
 import projectBiped from "../assets/project-icons/project-biped.png";
 import projectGemev from "../assets/project-icons/project-gemev.jpg";
-import projectTagAlong from "../assets/project-icons/project-tagalong.png";
+import projectTagAlong from "../assets/project-icons/project-tagalong-alt.png";
 import projectVisat from "../assets/project-icons/project-visat.png";
 import projectLlamaBear from "../assets/project-icons/project-llamabear.png";
 import projectIlliniAlert from "../assets/project-icons/project-illinialert.png";
+import React from "react";
 
 const SectionProjects = () => {
   return (
@@ -23,69 +24,85 @@ const SectionProjects = () => {
           align="center"
           sx={{ marginBottom: 2 }}
         >
-          Note: This section is a work in progress. In the near future, project
-          tiles will be clickable to display more information.
+          Selected Projects
         </Typography>
-        <Grid container spacing={0}>
-          <Grid item xs={12} sm={6} md={3}>
             <ProjectTile
               image={projectTagAlong}
               title={"TagAlong Robot"}
               description={
-                "User-following personal assistant for disabled users"
+                [  
+                  "User-following cargo carrying robot using a microcontroller for data processing and PID motor control with RaspberryPi for OpenCV tracking of the user in Cython",  
+                  "Designed modular main and breakout board PCBs in KiCAD, assembling boards using hand and reflow soldering",  
+                  "Engineered power subsystem for 3.3, 5 and 12V outputs from SLA battery using Buck converters and linear regulators",  
+                  "Awarded an Honorable Mention award for Spring 2023 Senior Design Project"  
+                ]  
               }
-              tags={["PCB Design"]}
+              tags={["PCB Design", "Computer Vision", "Circuit Assembly"]}
             />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
             <ProjectTile
               image={projectGemev}
               title={"Lane Following for GEM e2"}
-              description={"Lane following for GEM e2 electric vehicle"}
-              tags={["Python, OpenCV"]}
+              description={  
+                [  
+                  "Classical computer vision based autonomous lane-following system for a modified GEM e2 Electric Vehicle",  
+                  "Developed real-time OpenCV image processing for lane segmentation and center-of-lane path-planning",  
+                  "Utilized onboard RGBD stereo camera and GNSS antenna to plot GPS coordinates for use by onboard pure pursuit controller"  
+                ]  
+              }
+              tags={["Python", "Computer Vision", "OpenCV"]}
             />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
             <ProjectTile
               image={projectBiped}
-              title={"CPSIL - BIPED"}
+              title={"CPSIL - BIPED Project"}
               description={
-                "Two wheeled self-balancing robots for CS431 Embedded Systems and as a research platform for CPSIL Visat "
+                [  
+                  "Two-wheeled self-balancing robots designed for use by students in CS431 Embedded Systems and as a research platform for CPSIL Visat",  
+                  "Designed and assembled new IMU PCBs in KiCAD for Biped, a two-wheeled self-stabilizing research and educational robot",  
+                  "Executed comprehensive unit testing using oscilloscopes and multimeters to validate the functionality of Biped PCBs",  
+                  "Developed procedures to refurbish faulty hardware, software, and mechanical components, more than quadrupling the number of functional Biped units"  
+                ]                
               }
-              tags={["Python"]}
+              tags={["PCB Design", "Circuit Assembly"]}
             />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
             <ProjectTile
               image={projectVisat}
-              title={"CPSIL - VISAT"}
+              title={"CPSIL - VISAT Project"}
               description={
-                "User-following personal assistant for disabled users"
+                [  
+                  "Research group aimed at improving safety and robustness of next-generation autonomous vehicles under Professor Lui Sha",  
+                  "Implemented a PyTorch framework to evaluate the robustness of multitask learning and multi-layer perceptron networks to improve the safety of next-generation autonomous vehicles, increasing traffic sign classification accuracy by 7.96% and reducing loss of accuracy against ImageNet-C attacks by up to 17.83%",  
+                  "Applied sum-product network algorithm to improve classification accuracy of traffic signs affected by ImageNet-C corruptions and color quantization distribution shift",  
+                  "Engineered adversarial and distribution shift attacks using PyTorch and OpenCV to evaluate and improve robustness of vision pipelines",  
+                  "Applied multiprocessing to streamline experimental processes, reducing testing and processing run time by up to 65%"  
+                ]  
+                            
               }
               tags={["Python, Pytorch, OpenCV"]}
             />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
             <ProjectTile
               image={projectLlamaBear}
-              title={"LlamaBear"}
+              title={"Resume Review WebApp"}
               description={
-                "MERN stack web app for reviewing and improving resumes"
+                [  
+                  "MERN (MongoDB, Express JS, React, Node JS) stack web app that allows users to upload their resumes, tag them by fields, and receive as well as give anonymous reviews from other users and experts",  
+                  "Engineered a MongoDB backend and RESTful API using Express, Node JS, and Vercel to parse, store, retrieve, and display PDF files via Multer, and integrated with Firebase for user authentication and user data",  
+                  "Designed an interactive UI/UX prototype using Figma to gauge user feedback and iterate on design"  
+                ]                              
               }
               tags={["React, Node.js, Express, MongoDB, Firebase, Figma"]}
             />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3}>
             <ProjectTile
               image={projectIlliniAlert}
               title={"IlliniAlert App"}
               description={
-                "Functional prototype for a campus safety app, visualizing crimes and hazards relative to the user's location"
+                [  
+                  "Java-based Android app using Android Studio to enhance public safety through tracking of local emergencies",  
+                  "Visualized local emergencies identified from parsed SMS and email messages relative to user location using Google Map API",  
+                  "Conducted extensive user research, including over 24 interviews with targeted users at multiple stages of development, from initial user research and Figma UI prototype to functional prototype"  
+                ]  
               }
               tags={["Android Studio, Java, Google Maps API, Figma"]}
             />
-          </Grid>
-        </Grid>
       </StyleWrapper>
     </Container>
   );
@@ -97,9 +114,10 @@ export default SectionProjects;
 interface ProjectTileProps {
   image: string; // Imported images are strings of the filepath
   title: string;
-  description: string;
+  description: string[];
   tags: string[];
 }
+
 
 const ProjectTile: React.FC<ProjectTileProps> = ({
   image,
@@ -108,70 +126,118 @@ const ProjectTile: React.FC<ProjectTileProps> = ({
   tags,
 }) => {
   return (
-    <Box
-      id="projectContainer"
-      sx={{
-        position: "relative",
-        width: "100%",
-        paddingTop: "100%", // 1:1 aspect ratio
-        overflow: "hidden",
-      }}
-    >
-      <StyleWrapper>
-        <Box
-          id="projectImage"
-          component="img"
-          src={image}
-          alt={title}
+    <Box display="flex" flexDirection="row" id="projectContainer"
           sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            width: "100%",
-            height: "100%",
-            objectFit: "contain",
-            transform: "translate(-50%, -50%)",
-          }}
-        />
-        <Box
-          id="projectHoverSection"
-          sx={{
-            position: "absolute",
-            top: 0,
-            background: "rgba(0, 0, 0, 0.5)",
-            color: "white",
-            boxSizing: "border-box",
-            height: "100%",
-            width: "100%",
-            opacity: 0,
-            padding: "5%",
-            transition: "opacity 0.3s",
-            "&:hover": {
-              opacity: 1,
+            "& > *": {
+              px: "1rem"
             },
-            textWrap: "wrap",
+            py: "0.5rem",
           }}
-        >
-          <Typography
-            variant="h4"
-            className="fontPrimary textBold textSecondary"
-          >
-            {title}
-          </Typography>
-          <Typography variant="h6" className="fontPrimary textSecondary">
-            {description}
-          </Typography>
-          <Typography
-            variant="body2"
-            className="fontPrimary textSecondary textUnderline textBold"
-          >
-            Tags:
-          </Typography>
-          <Typography variant="body2" className="fontPrimary textSecondary">
+    >
+      <Box
+        id="projectImage"
+        component="img"
+        src={image}
+        alt={title}
+        sx={{
+          width: "25%",
+        }}
+      />
+      <Box id="projectText"
+          sx={{
+            "& > *": {
+              py: "0.15rem"
+            },
+          }}
+      >
+        <Typography variant="h5" className="textBold fontPrimary">
+          {title}
+        </Typography>
+        <Typography variant="body1" className="textRegular fontPrimary">
+          {description.map((item, index) => (
+            <React.Fragment key={index}>
+              {"‣ "} {item}
+              {index < description.length - 1 && <br />}
+            </React.Fragment>
+          ))}
+        </Typography>
+        <Typography variant="body2" className="textRegular fontPrimary">
+            {"Relevant Skills: "}
             {tags.join(", ")}
-          </Typography>
-        </Box>
-      </StyleWrapper>
+        </Typography>
+      </Box>
     </Box>
   );
 };
+
+
+// LEGACY
+
+
+// const ProjectTile: React.FC<ProjectTileProps> = ({
+//   image,
+//   title,
+//   description,
+//   tags,
+// }) => {
+//   return (
+
+//     <Box
+//       id="projectContainer"
+//       sx={{
+//         "& > *": {
+//           py: "0.15rem",
+//         },
+//         py: "0.5rem",
+//         overflow: "hidden",
+//       }}
+//     >
+//       <StyleWrapper>
+//         <Box
+//           id="projectImage"
+//           component="img"
+//           src={image}
+//           alt={title}
+//           sx={{
+//             position: "absolute",
+//             top: "50%",
+//             left: "0%",
+//             width: "25%",
+//             height: "100%",
+//             objectFit: "contain",
+//             transform: "translate(-50%, -50%)",
+//           }}
+//         />
+//         <Box
+//           id="projectHoverSection"
+//           sx={{
+//             position: "absolute",
+//             top: 0,
+//             height: "100%",
+//             width: "75%",
+//             textWrap: "wrap",
+//           }}
+//         >
+//           <Typography
+//             variant="h4"
+//             className="fontPrimary textBold textSecondary"
+//           >
+//             {title}
+//           </Typography>
+//           <Typography variant="h6" className="fontPrimary textSecondary">
+//             {description}
+//           </Typography>
+//           <Typography
+//             variant="body2"
+//             className="fontPrimary textSecondary textUnderline textBold"
+//           >
+//             Tags:
+//           </Typography>
+//           <Typography variant="body2" className="fontPrimary textSecondary">
+//             {tags.join(", ")}
+//           </Typography>
+//         </Box>
+//       </StyleWrapper>
+//     </Box>
+//   );
+// };
